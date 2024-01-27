ebd113ef19317a4d9001a1268a1bc8f26550cee7
snehadomadiya <snehadomadiya1617@gmail.com>
2024-01-28 00:13:13 +0100

    data types

---
+++ type.js
@@ -0,0 +1,9 @@
+var firstName = "sneha";
+console.log(firstName);
+var age = 30;
+console.log(age);
+var check = true;
+console.log(check);
+var myList = ["sneha"];
+myList.push("hiyan");
+console.log(myList);
---
+++ type.ts
@@ -0,0 +1,14 @@
+let firstName: string = "sneha";
+
+console.log(firstName);
+
+let age: number = 30;
+console.log(age);
+
+let check: boolean = true;
+console.log(check);
+
+const myList: string[] = ["sneha"];
+myList.push("hiyan")
+myList.push("chirag")
+console.log(myList)
